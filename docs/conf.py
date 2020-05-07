392fdc681e4a33bc13e7dff7792bd9412dfcdbf2
Jesse Griffin <jag3773@gmail.com>
2020-05-07 12:58:58 -0400

    Trying to fix borders on schema tables

--- docs/conf.py
+++ docs/conf.py
@@ -158,6 +158,12 @@ todo_include_todos = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
